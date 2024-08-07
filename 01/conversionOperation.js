ce2360d39474e1561eaf9477a7ac52503844f766
SAIYAM1243 <109846906+SAIYAM1243@users.noreply.github.com>
2024-08-07 13:59:19 +0530

    conversion of data types

---
+++ 01/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = ""
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "69" => 69
+// "69abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "saiyam" => true
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
